07ec399d045e5cf67da94b21ae3bb5030029d24e
Josmar Perez <craftero350@gmail.com>
2025-10-01 22:02:15 -0600

    ACT 01/10/25 - 10:02

--- 3MLIDTS-JosmarFernandez-04cs(NEW)/Form1.cs
+++ 3MLIDTS-JosmarFernandez-04cs(NEW)/Form1.cs
@@ -48,25 +48,60 @@ namespace _3MLIDTS_JosmarFernandez_04cs_NEW_
         {
             return Regex.IsMatch(texto, @"^[a-zA-Z\s]+$");
         }
-
+        private bool EsEstaturaVálida(string valor)
+        {
+            decimal resultado;
+            return decimal.TryParse(valor, out resultado);
+            //return false;
+        }
+        private bool EdadVálida(string valor)
+        {
+            int resultado;
+            return int.TryParse(valor, out resultado);
+            //return false;
+        }
+        private bool ValidoEntero10Dígitos(string valor)
+        {
+            return valor.Length == 10 && valor.All(char.IsDigit);
+        }
         private void validarEdad(object sender, EventArgs e)
         {
-
+            TextBox textBox = (TextBox)sender;
+            if (!EdadVálida(textBox.Text))
+            {
+                MessageBox.Show("Ingrese un valor decimal válido para la edad", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
         }
 
         private void validarEstatura(object sender, EventArgs e)
         {
-
+            TextBox textBox = (TextBox)sender;
+            if (!EsEstaturaVálida(textBox.Text))
+            {
+                MessageBox.Show("Ingrese un valor decimal válido para la estatura", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
         }
 
         private void validarApellido(object sender, EventArgs e)
         {
-
+            TextBox textBox = (TextBox)sender;
+            if (!EsTextoValido(textBox.Text))
+            {
+                MessageBox.Show("Ingrese valores válidos para el apellido", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
         }
 
         private void validarTelefono(object sender, EventArgs e)
         {
-
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text.Length == 10 && !ValidoEntero10Dígitos(textBox.Text))
+            {
+                MessageBox.Show("Ingrese valores válidos para el Teléfono", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
         }
 
         private void btnGuardar_Click(object sender, EventArgs e)
@@ -88,7 +123,7 @@ namespace _3MLIDTS_JosmarFernandez_04cs_NEW_
             }
             if (!string.IsNullOrEmpty(txtEdad.Text) || !string.IsNullOrEmpty(txtEstatura.Text) || !string.IsNullOrEmpty(txtNombre.Text) || !string.IsNullOrEmpty(txtTeléfono.Text) || !string.IsNullOrEmpty(txtApellidos.Text))
             {
-                String datos = $"Nombre:{name}\n\rApellido:{apellido}\n\r Edad:{edad} \n\r Altura: {est} \n\r Telefono: {num} \n\r Sex: {genero}";
+                string datos = $"Nombre:{name}\n\rApellido:{apellido}\n\r Edad:{edad} \n\r Altura: {est} \n\r Telefono: {num} \n\r Sex: {genero}";
                 string ruta = @"C:\Users\craft\Documents\Códigos\txt\Datos3MAgosto2025.txt";
                 bool archivoExt = File.Exists(ruta);
                 using (StreamWriter writer = new StreamWriter(ruta, true))
@@ -106,7 +141,6 @@ namespace _3MLIDTS_JosmarFernandez_04cs_NEW_
 
                 // String datos = $"Nombre:{name}\n\rAPellido:{sur}\n\r Edad:{age} \n\r Altura: {altura} \n\r Telefono: {cel} \n\r Sex: {genero}";
                 MessageBox.Show("ingrese valores a los espacios en blanco", "error", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
             }
         }
     }
